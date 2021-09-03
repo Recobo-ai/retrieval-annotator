03bc416488e5db1b39ad3c69c460d3162df3d930
shahrukhx01 <sk28671@gmail.com>
2021-09-03 15:00:20 +0200

    update gitignore

--- display_df.py
+++ display_df.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 annotator_name = "Shahrukh Khan"
-annotator_file_name = "auto-mpg.csv"
+annotator_file_name = "generated_queries.csv"
 
 # Number of entries per screen
 N = 2
@@ -14,9 +14,9 @@ st.markdown(f" {annotator_name} @ Chemical Data Annotations")
 # A variable to keep track of which product we are currently displaying
 session_state = SessionState.get(page_number=0)
 
-data = pd.read_csv(annotator_file_name)
+data = pd.read_csv(annotator_file_name)[33000:66000]
 last_page = len(data) // N
-
+st.write(f"Page: {session_state.page_number}/{last_page}")
 # Add a next button and a previous button
 
 prev, _, next = st.beta_columns([1, 10, 1])
@@ -39,16 +39,17 @@ if prev.button("Previous"):
 start_idx = session_state.page_number * N
 end_idx = (1 + session_state.page_number) * N
 
+## global checkbox
+all_checkbox = st.checkbox("Select/De-Select all")
+
 # Index into the sub dataframe
 sub_df = data.iloc[start_idx:end_idx]
 col1, col2 = st.beta_columns((1, 9))
 col3, col4 = st.beta_columns((1, 9))
 
-question_1_marked = col1.checkbox("")
-question_1 = col2.text_input("Questions", value=sub_df.iloc[0]["mpg"])
-question_2_marked = col3.checkbox(" ")
-question_2 = col4.text_input("", value=sub_df.iloc[1]["mpg"])
-
-context = st.write(
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-)
+question_1_marked = col1.checkbox("", value=all_checkbox)
+question_1 = col2.text_input("Questions", value=sub_df.iloc[0]["question"])
+question_2_marked = col3.checkbox(" ", value=all_checkbox)
+question_2 = col4.text_input("", value=sub_df.iloc[1]["question"])
+if sub_df.iloc[0]["context"] == sub_df.iloc[1]["context"]:
+    context = st.write(sub_df.iloc[0]["context"])
