731439ff6428df658ecc9de48ae5de5fe9990ed3
shahrukhx01 <sk28671@gmail.com>
2021-09-10 18:37:45 +0200

    add heroku deployment files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 mkdir -p ~/.streamlit/
+wget https://media.githubusercontent.com/media/shahrukhx01/retrieval-annotator/main/generated_queries_emerson.csv -O generated_queries_emerson1.csv
 echo "\
 [server]\n\
 headless = true\n\
