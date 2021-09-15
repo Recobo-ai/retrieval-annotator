4772ce0246a0933f4ef7ba12746295d773a0fdd9
shahrukhx01 <sk28671@gmail.com>
2021-09-15 07:50:26 +0200

    update annotation data source

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 mkdir -p ~/.streamlit/
-wget https://docs.google.com/spreadsheets/d/1kGPY2inrIc05Ik7162TkmuHRrWOlSDy8aFjKkUw0M8w/export?format=csv -O generated_queries_praveena1.csv
 echo "\
 [server]\n\
 headless = true\n\
